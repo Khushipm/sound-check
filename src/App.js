b698ce755b45588eaeffd3f89037cdb10c7a1c10
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2021-02-15 19:17:35 +0000

    Working Beep!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import useSound from "use-sound";
+import beepSfx from "./beep.mp3";
 import "./styles.css";
 
 export default function App() {
-  const [play] = useSound("beep.mp3");
+  const [play] = useSound(beepSfx);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={play} className="btn btn-danger">
+        Dei Venna
+      </button>
     </div>
   );
 }
